29b557c310a1e14b2f945bc23339508e6dd80229
Irani-putri <iraniputri347@gmail.com>
2025-05-07 20:33:05 +0700

    Add files via upload

---
+++ ProjectPraktikumBab6/src/Transportasi/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author Irani Putri
+ */
+public class Main {
+    public static void main(String[] args) {
+        TiketEkonomi tiket1 = new TiketEkonomi("TK001", "Andi Saputra", "Jakarta - Bali", "10 Maret 2025", 1500000, true);
+        TiketBisnis tiket2 = new TiketBisnis("TK002", "Siti Aminah", "Jakarta - Surabaya", "12 Maret 2025", 2500000, "Lounge VIP dan Makanan Gratis");
+
+        System.out.println("=== Informasi Tiket Lengkap ===");
+        // Panggil method langsung sesuai dengan tipe objeknya
+        System.out.println(tiket1.getInfoTiket());
+        System.out.println(tiket2.getInfoTiket());
+
+        System.out.println("=== Informasi Tiket Ringkas (Overloading) ===");
+        System.out.println(tiket1.getInfoTiket(true));
+        System.out.println(tiket2.getInfoTiket(true));
+    }
+}
+
---
+++ ProjectPraktikumBab6/src/Transportasi/TiketBisnis.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Transportasi;
+
+/**
+ *
+ * @author Irani Putri
+ */
+
+public class TiketBisnis extends TiketPesawat {
+    private String fasilitasTambahan;
+
+    public TiketBisnis(String kodeTiket, String namaPenumpang, String tujuan, String tanggalKeberangkatan, double harga, String fasilitasTambahan) {
+        super(kodeTiket, namaPenumpang, tujuan, tanggalKeberangkatan, harga);
+        this.fasilitasTambahan = fasilitasTambahan;
+    }
+
+    @Override
+    public String getInfoTiket() {
+        return "Kode Tiket             : " + getKodeTiket() + "\n" +
+               "Nama Penumpang         : " + getNamaPenumpang() + "\n" +
+               "Tujuan                 : " + getTujuan() + "\n" +
+               "Tanggal Keberangkatan  : " + getTanggalKeberangkatan() + "\n" +
+               "Harga Tiket            : Rp " + getHarga() + "\n" +
+               "Fasilitas Tambahan     : " + fasilitasTambahan + "\n";
+    }
+
+    @Override
+    public String getInfoTiket(boolean ringkas) {
+        if (ringkas) {
+            return getKodeTiket() + " - " + getNamaPenumpang() + " [Bisnis]";
+        } else {
+            return getInfoTiket();
+        }
+    }
+
+    Object getFasilitasTambahan() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
+
